2b56cd4e481c7644768ef93f668b5af4a165d5e9
vaibhav <vaibhavpalve1234@gmail.com>
2021-01-06 12:36:29 +0530

    day1

---
+++ practise.js
@@ -0,0 +1,72 @@
+// console.log("hello world");
+// var b = " something";
+// console.log(b);
+// document.getElementById("some").innerHTML = " helloworld";
+//  var age = prompt("what is your age?");
+// document.getElementById("some").innerHTML= age;
+
+// numbers
+
+// var number = 65
+// var send = 76 
+// console.log(number + send);
+// document.write(2121);
+
+
+// console.log(5+10);
+// var num = 10 ;
+// num --;
+// console.log(num)
+// num ++;console.log(num)
+// num *= 2;console.log(num)
+
+
+//function
+
+
+// function fun(){
+//     console.log("my class");
+// }
+// fun();
+
+
+// function griding(){
+//     var name = prompt("what is you name?");
+//     var result = "hello" + " "+name ;
+//     console.log(result);
+// }
+// griding();
+
+
+/*
+not working
+
+function twoSum(num1,num2){
+    var result = num1 + num2 ;
+    console.log(result);
+    
+}
+
+var number1 = prompt();
+var number2 = prompt();
+twoSum(number1,number2);*/
+// function greeting(yourName){
+//     var result = "hello" + " "+ yourName;
+//     console.log(result);
+    
+// }
+// var a = prompt("enter a name :");
+// greeting(a);
+
+// var num = 0;
+// while (num < 5){
+//     num += 1;
+//     console.log(num);
+// }
+
+// for (var index = 0; index < 10; index++) {
+//     console.log(index);
+    
+// }
+
+//datatype
