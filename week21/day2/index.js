cdcd6ae8753469807b79d9e2be98d6a72f140238
vaibhav <vaibhavpalve1234@gmail.com>
2021-01-12 18:24:06 +0530

    practice

---
+++ week21/day2/index.js
@@ -0,0 +1,9 @@
+var name1  = "vaibhav" ;
+ var about="i am engineer and persunging last year";
+
+console.log(`hello my name is ${name1} and late understand about me ${about}`);
+console.log(2+5);
+ 
+
+const fs = require("fs");
+fs.writeFileSync("read.txt","welcome to my rggfg");
