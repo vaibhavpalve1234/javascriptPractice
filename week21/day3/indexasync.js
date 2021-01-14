55f901d86adc626c7efacdb190688dea124098e6
vaibhav <vaibhavpalve1234@gmail.com>
2021-01-14 12:22:00 +0530

    day

--- week21/day3/indexasync.js
+++ week21/day3/indexasync.js
@@ -1,8 +1,20 @@
 const fs = require("fs");
-fs.mkdir("vap",(err)=>{
-    console.log("complted")
-});
+// fs.mkdir("vap",(err)=>{
+//     console.log("complted")
+// });
 
-fs.writeFile("vap/read.txt","my name is vaibhav",(err)=>{
-    console.log("complted");
-});
+// fs.writeFile("vap/read.txt","my name is vaibhav",(err)=>{
+//     console.log("complted");
+// });
+// fs.appendFile("vap/read.txt","my 7776090314",(err)=>{
+//     console.log("complted");
+// });
+// fs.readFile("./vap/read.txt",'utf-8', (err,data)=>{
+//     console.log(data);
+// });
+// fs.rm("./vap/read.txt",(err)=>{
+//     console.log("file deleted");
+// });
+// fs.rmdir("./vap",(err)=>{
+//     console.log("completed");
+// });
