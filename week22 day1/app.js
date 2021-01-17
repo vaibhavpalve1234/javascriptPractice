322f19d9a808a7aa90e888eef4cf5dd65ed2be49
vaibhav <vaibhavpalve1234@gmail.com>
2021-01-17 15:16:39 +0530

    day1

--- week22 day1/app.js
+++ week22 day1/app.js
@@ -1,16 +1,8 @@
-const express = require("express");
-const app = express();
-
-app.get("/",(req,resp)=>{
-    resp.send("hello world")
-});
-
-app.get("/aline",(req,resp)=>{
-    // const id = req.query.id
-    resp.send("hello student");
-});
-app.listen(9987, (req,resp)=>{
-    console.log("rumming.. ");
-    console.log("rumming.. "+ req);
+const http = require("http");
+const server = http.createServer((req,resp)=>{
+    resp.end("completed");
     
+});
+server.listen(3000,(req,resp)=>{
+    console.log(`http://localhost:${3000}`);
 });
