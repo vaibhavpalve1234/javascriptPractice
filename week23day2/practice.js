6fc701a756286ca2d61cc452d59ff742bba3a12c
vaibhav <vaibhavpalve1234@gmail.com>
2021-01-27 00:07:26 +0530

    day1
    Signed-off-by: vaibhav <vaibhavpalve1234@gmail.com>

--- week23day2/practice.js
+++ week23day2/practice.js
@@ -1,17 +1,38 @@
 const express = require('express');
 const path = require('path');
 const app = express()
+const bodyParser = require("body-parser");
+const { userInfo } = require('os');
 const port = 8000;
+let use = [];
+let count = {};
 // const staticsPath = path.join(__dirname,'../public');
-const templatePath = path.join(__dirname,"../templates")
+// const templatePath = path.join(__dirname,"../templates")
 app.set('view engine', 'hbs');
-app.set('views',templatePath)
+// app.set('views',templatePath);
+app.use(bodyParser.json());
 
 // app.use(express.static(staticsPath));
 app.get("/",(req,res)=>{
+
     res.render('index',{ 
         name : "vaibhav"
     })
+})
+app.post("/home",(req,res)=>{
+    console.log(req.body);
+    use.push(req.body)
+    
+    res.json({
+        massage:"welcome to your home"
+    })
+})
+app.get("/data",(req,res)=>{
+
+    res.json(use);
+})
+app.get("/count",(req,res)=>{
+    res.json()
 })
 app.listen(port,()=>{
     console.log(`server live on http://localhost:${port}`)
