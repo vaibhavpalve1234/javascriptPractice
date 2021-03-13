5e985160fd0a888cfa20b81ac148b60f612a9ba8
vaibhav <vaibhavpalve1234@gmail.com>
2021-03-14 01:06:48 +0530

    bad time
    Signed-off-by: vaibhav <vaibhavpalve1234@gmail.com>

--- feb25/weatherapplication/server.js
+++ feb25/weatherapplication/server.js
@@ -4,11 +4,14 @@ const https = require('https');
 const app = express ();
 
 app.get('/',(req,res)=>{
-    https.get('https://api.openweathermap.org/data/2.5/weather', (res)=>{
+    https.get('https://api.weatherapi.com/v1/current.json?key=2bdc5a8aac3e4d7782934959212602&q=mumbai&aqi=no', (res)=>{
+        console.log(res.statusCode)
         
     res.on('data',(data)=>{
        const weatherdata = JSON.parse(data)
        console.log(weatherdata);
+       const temp = weatherdata.main.temp
+       const weatherDescription = weatherdata.weather[0].description 
     })
       
     })
